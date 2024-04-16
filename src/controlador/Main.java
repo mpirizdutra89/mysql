b96790b95cb288e73ff9dea6f2101d9e0743c294
mpirizdutra89 <163307010+mpirizdutra89@users.noreply.github.com>
2024-04-16 15:45:40 -0300

    armado

---
+++ src/controlador/Main.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package controlador;
+
+/**
+ *
+ * @author nn
+ */
+public class Main {
+
+   
+    public static void main(String[] args) {
+      Db.prueba(Db.consulta("select usuario_id,grupos_idgrupos, usuario_nombre,usuario_password from usuarios;"));
+       // Db.servicioMysql();
+       Db.getDesconexion();
+    }
+    
+}
